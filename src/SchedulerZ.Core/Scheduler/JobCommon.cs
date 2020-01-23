a4be5499e3ac57eac15b26be4725de8a7c0b3836
zhangkong <602488225@qq.com>
2020-01-23 21:33:39 +0800

    * JobCommon.cs:
    * JobView.cs:

---
+++ src/SchedulerZ.Core/Scheduler/JobCommon.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Quartz;
+
+namespace SchedulerZ.Core.Scheduler
+{
+    [PersistJobDataAfterExecution]
+    [DisallowConcurrentExecution]
+    public class JobCommon:IJob
+    {
+        public JobCommon()
+        {
+        }
+
+        public Task Execute(IJobExecutionContext context)
+        {
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ src/SchedulerZ.Core/Scheduler/JobView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+namespace SchedulerZ.Core.Scheduler
+{
+    public class JobView
+    {
+        public JobView()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Remark { get; set; }
+        public bool IsSimple { get; set; } = false;
+        public string CronExpression { get; set; }
+        public int RepeatCount { get; set; } = -1;
+        public TimeSpan IntervalTimeSpan { get; set; } = TimeSpan.FromSeconds(0);
+        public string AssemblyName { get; set; }
+        public string ClassName { get; set; }
+        public DateTime? StartTime { get; set; }
+        public DateTime? EndTime { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        public bool Validate()
+        {
+            if (string.IsNullOrWhiteSpace(this.Name))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(this.AssemblyName) || string.IsNullOrWhiteSpace(this.ClassName))
+                return false;
+
+            if (this.IsSimple)
+            {
+                if (this.RepeatCount != -1 && this.RepeatCount <= 0)
+                    return false;
+
+                if (this.IntervalTimeSpan < TimeSpan.FromSeconds(0))
+                    return false;
+            }
+            else
+            {
+                if (string.IsNullOrWhiteSpace(this.CronExpression))
+                    return false;
+                return SchedulerManager.ValidExpression(this.CronExpression);
+            }
+
+            return true;
+        }
+    }
+}
